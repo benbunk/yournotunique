284ac37b5e7a87e5f4da05b0e922822dfbe62fa1
benbunk <ben+github@stupil.com>
2013-07-13 14:14:02 -0400

    Changed line endings to unix.

--- searcher.c
+++ searcher.c
@@ -11,15 +11,21 @@
  */
 
 // Headers
-#include <stdio.h> // cin, cout
+#include <stdio.h> // printf
 #include <string.h> // memcmp
 #include <dirent.h> // struct dirent, type DIR
+#include <stdbool.h> // bool
 
 
 // Function Prototypes
+// @todo - Switch the size int to size_t for passing array size around.
 int convert(char *, int);
-int r_search(char *, unsigned char *, int);
-int search(char *, unsigned char *, int);
+int findInBuffer(unsigned char *, size_t, unsigned char *, size_t);
+int search(char *, unsigned char *, size_t);
+int r_search(char *, unsigned char *, size_t);
+void printHexBuffer(unsigned char *, size_t);
+
+int g_hit_counter = 0;
 
 /**
  * Main entry point.
@@ -48,7 +54,7 @@ int main(int argc, char *argv[])
       i++;
     }
     
-    int buffer_length = i - 2;
+    size_t buffer_length = i - 2;
     
     // Call the appropriate search method.
     if ( strcmp(filename, "r") == 0 ) {
@@ -89,12 +95,137 @@ int convert(char *buffer, int cbase) {
 }
 
 /**
+ * Find a value from one buffer in the other.
+ */
+int findInBuffer(unsigned char *search_buffer, size_t search_len, unsigned char *window_buffer, size_t window_len) {
+  int i;
+  int foundAt = 0;
+  int search = 0;
+  bool found = false;
+
+  for (i = 0; i < window_len && search < search_len; i++) {
+  // If we find the match
+    if (window_buffer[i] == search_buffer[search]) {
+      if (found == false) {
+        found = true;
+        foundAt = i;
+      }
+      search++;
+    }
+    else {
+      if (found == true) {
+        found = false;
+        search--;
+        i = foundAt;
+      }
+    }
+  }
+
+  if (found == true) {
+    return foundAt;
+  }
+  else {
+    return -1;
+  }
+}
+
+/**
+ * Search for a hex pattern in a given file.
+ */
+int search(char *filename, unsigned char *input_buffer, size_t buffer_length) {
+  int i = 0;
+  int err = 0;
+  int ret_val = 0;
+  
+  FILE *file;
+  size_t bytes_read;
+  int total_bytes_read = 0; // @todo make this size_t
+  unsigned char buffer[buffer_length];
+
+  printf("\nSearching: %s ", filename);
+
+  // Get the file handle.
+  if ((file = fopen(filename, "rb")) == NULL) {
+    printf("Failed: %s\n", filename); 
+    // @todo - Why is this a 1? ... probably a particular error code. No convention set though. ... Possibly for the recursive version.
+    return ret_val = 1;
+  }
+
+  //Process the file for matches
+  do {
+    // If we didn't find a match check the current buffer for a place to start searching.
+    // @todo - Make this it's own function.
+    // @todo - Change to search from first occurrence to the end of the buffer.
+      // @todo - Peak ahead could be a bit of a compromise.
+    // @todo - Check if memchr is sufficient for this. @see http://www.cplusplus.com/reference/cstring/memchr/
+    if (total_bytes_read > 0) {
+
+      //Search the current array for another occurance of the first character
+      // @todo - bytes_read is probably better.
+      bool match = false;
+      for (i = 0; i < buffer_length; i++) {
+        if (input_buffer[0] == buffer[i]) {
+          total_bytes_read -= buffer_length;
+          total_bytes_read += i + 1;
+
+          // Move the read head to the position we found.
+          fseek(
+            file,
+            total_bytes_read, // Num bytes to rewind -- we are actually fast forwarding from the beginning, thus total.
+            SEEK_SET          // Where to start from (SEEK_SET, SEEK_CUR, SEEK_END).
+          );
+
+          break;
+        }
+      }
+    }
+
+    // Pull in a new buffer based on where the read head is.
+    // @see fseek().
+    bytes_read = fread(
+      buffer,        // Fill this with the data being read.
+      1,             // How big is an item.
+      buffer_length, // How many items to read.
+      file           // The file to read from.
+    );
+
+    // Break at the end of the file.
+    if(bytes_read < buffer_length) {
+      err = 1;
+      break;
+    }
+
+    // Increment the Byte counter.
+    // @todo - This is sending a wrong total bytes if the EOF is reached. ... Try bytes_read instead.
+    total_bytes_read += buffer_length;
+  }
+  while(memcmp(input_buffer, buffer, bytes_read) != 0);
+  
+  // Close the file handle.
+  fclose(file);
+
+  // If Success.
+  if (err != 1) {
+    ret_val = 1; // @todo this is the same code as a bad file handle.
+
+    printf("\nSuccess!\n  Input:  ");
+    printHexBuffer(input_buffer, buffer_length);
+    printf("\n  Output: ");
+    printHexBuffer(buffer, bytes_read);
+    printf("\n  Position: %d", total_bytes_read);
+  }
+
+  return ret_val;
+}
+
+/**
  * Run a search recursively against the speicifed folder instad of on a single file.
  * @see search().
  *
- * @todo - File and directory handling is off...maybe im not doing it posix style?
+ * @todo - Handle file permissions.
+ * @todo - Skip locked files.
  */
-int r_search(char *starting_directory, unsigned char *input_buffer, int buffer_length) {
+int r_search(char *starting_directory, unsigned char *input_buffer, size_t buffer_length) {
   int ret_val = 0;
   DIR *directory = NULL;
   struct dirent *dir = NULL;
@@ -132,12 +263,15 @@ int r_search(char *starting_directory, unsigned char *input_buffer, int buffer_l
           // No Op.
         } 
         else {
-          printf("Calling r_search: starting_directory = %s | path = %s\n===========\n", starting_directory, path);
+          printf("\nCalling r_search: starting_directory = %s | path = %s\n===========\n", starting_directory, path);
           ret_val = r_search(path, input_buffer, buffer_length);
           
           // ENABLE THIS TO STOP ON THE FIRST FOUND MATCH.
           // CURRENTLY WE ARE TESTING TO SEE HOW MANY MATCHES WE CAN FIND.
-          // if (ret_val == 1) { break; }
+          if (ret_val == 1) {
+            //break;
+            printf("\n\n\n Number of matches: %d", ++g_hit_counter);
+          }
         }
 
       // Other possibilities but we don't care
@@ -154,109 +288,17 @@ int r_search(char *starting_directory, unsigned char *input_buffer, int buffer_l
 }
 
 /**
- * Search for a hex pattern in a given file.
+ * Print Hex Buffer helper method.
  */
-int search(char *filename, unsigned char *input_buffer, int buffer_length) {
-  FILE *file;
-  int ret_val = 0;
-  
-  if ((file = fopen(filename, "rb")) == NULL) {
-    printf("Failed: %s\n", filename); 
-    // @todo - Why is this a 1?
-    return ret_val = 1;
+void printHexBuffer (unsigned char *buffer, size_t len) {
+  int i;
+  for (i = 0; i < len; i++) {
+    printf("%.2X ", buffer[i]);
   }
-  else {
-    int i = 0;
-    size_t bytes_read;
-    unsigned char buffer[buffer_length];
-    int err = 0;
-    int j = 0;
-    int total_bytes_read = 0;
-
-    printf("Searching: %s ", filename);
-
-    //Process the file for matches
-    do {
-      if (j > 0) {
-      // if buffer_length == numItemsCurrentPass || numItemsCurrentPass > lowerLimit
-      // Reset pointer back to beginning 
-      // Set numItemsCurrentPass 1 less than last time
-      // Search again
-      // else 
-      // reset back to beginning
-      // search original buffer based on original size for another occurence of first char
-      // Try it over again. 
-
-        //Search the current array for another occurance of the first character
-        for (i = 0; i < buffer_length; i++) {
-          if (input_buffer[0] == buffer[i]) {
-            // If it exists seek from the beginning multiply the number of passes by the buffer_length to get total bytes read.
-            // so far. 
-            // Than add the array index to set the pointer to the current location of the found byte.
-            total_bytes_read -= buffer_length;
-            total_bytes_read += i + 1;
-
-            fseek(
-              file,
-              total_bytes_read, // Num bytes to rewind -- we are actually fast forwarding from the beginning, thus total.
-              SEEK_SET          // Where to start from (SEEK_SET, SEEK_CUR, SEEK_END).
-            );
-
-            break;
-          }
-        }
-      }
-
-      bytes_read = fread(
-        buffer,   // Fill this with the data.
-        1,        // How big is an item.
-        buffer_length, // How many items to read.
-        file);    // The file to read from.
-
-      // Breaking here eliminates the need for a condition in the do{}while();
-      if(bytes_read < buffer_length) {
-        err = 1;
-        break;
-      }
-
-      j++;
-      total_bytes_read += buffer_length;
-    }
-    while(memcmp(input_buffer, buffer, bytes_read) != 0);
-
-    if (err == 1) {
-      // No Op.
-    }
-    else {
-      ret_val = 1;
-      
-      printf("\n%s\nSuccess:\nInput:  ", filename);
-      
-      for (i = 0; i < buffer_length; i++) {
-        printf("%.2X ", input_buffer[i]);
-      }
-      
-      printf("\nOutput: ");        
-
-      for(i = 0; i < bytes_read; i++) {
-        printf("%.2X ", buffer[i]);
-      }
-
-      printf("\nPosition: %d", total_bytes_read);
-    }
-    
-    fclose(file);
-  }
-
-  return ret_val;
+  return;
 }
 
 /*
-
-int findInBuffer (searchBuffer, searchValue) { 
- 
- return positionSearchValue || -1;
-}
 
 int rewindReadHead (file, totalBytes, numBytesToRewind) {
   
@@ -264,4 +306,4 @@ int rewindReadHead (file, totalBytes, numBytesToRewind) {
 }
 
 
-*/
+// */
