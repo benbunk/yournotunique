4171d14e94ec4761c2f3aba772bdf2856c119e30
benbunk <ben+github@stupil.com>
2013-07-18 22:50:54 -0400

    Notes about IPC message queues and examples for later.

--- pthread.c
+++ pthread.c
@@ -1,6 +1,10 @@
 #include <pthread.h>
 #include <stdio.h>
 
+// An example of pthread and ipc for messaging.
+// http://stackoverflow.com/questions/5641483/thread-grabs-other-threads-ipc-message-from-queue-linux
+// http://www.cs.cf.ac.uk/Dave/C/node25.html
+
 /* This is our thread function.  It is like main(), but for a thread*/
 void *threadFunc(void *arg)
 {
